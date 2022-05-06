b1849c04ba679e2e188dce3e8807ddcf32629b3a
MIS <amokraneakram54@gmail.com>
2022-05-06 14:31:41 +0100

    spamer

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +15,7 @@ public class Main extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("/com/example/demo/logo.png")));
         stage.show();
     }
 
